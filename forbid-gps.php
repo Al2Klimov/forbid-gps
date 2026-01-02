8b2ca9a72837c9df474dd5a46aeaee21e9298af7
Alexander A. Klimov <a@kli.mov>
2026-01-02 22:33:00 +0100

    WP plugin header

---
+++ forbid-gps.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Plugin Name:         Forbid GPS
+ * Plugin URI:          https://github.com/Al2Klimov/forbid-gps
+ * Description:         Prevents photos containing geographic location data from being uploaded.
+ * Version:             1.0
+ * Author:              Alexander A. Klimov
+ * Author URI:          https://github.com/Al2Klimov
+ * License:             GPL v2 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:         forbid-gps
+ */
